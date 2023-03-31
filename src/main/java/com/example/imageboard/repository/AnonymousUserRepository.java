38c5441b85b8d7bf4420f894cd30131dc9aa3f0a
behem0thik <behem0thik@users.noreply.github.com>
2023-03-31 19:27:45 +0600

    delete User Repository

--- src/main/java/com/example/imageboard/repository/AnonymousUserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.imageboard.repository;
-
-import com.example.imageboard.model.AnonymousUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AnonymousUserRepository extends JpaRepository<AnonymousUser, Long> {
-}
