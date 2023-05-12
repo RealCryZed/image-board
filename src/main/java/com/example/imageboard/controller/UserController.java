5aacc05c3e9a7f1bc52e63690db6a39041c4931e
RealCryZed <bujhmgtnhjd@bk.ru>
2023-05-12 23:35:35 +0600

    Added working register page, login still doesn't work

--- src/main/java/com/example/imageboard/controller/UserController.java
+++ src/main/java/com/example/imageboard/controller/UserController.java
@@ -2,6 +2,7 @@ package com.example.imageboard.controller;
 
 import com.example.imageboard.model.User;
 import com.example.imageboard.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
@@ -11,6 +12,7 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.validation.Valid;
 
 @Controller
+@Slf4j
 public class UserController {
 
     @Autowired
@@ -19,17 +21,24 @@ public class UserController {
     @PostMapping("/register")
     public ModelAndView createNewUser (ModelAndView modelAndView,
                                        @Valid User user, BindingResult bindingResult) {
+        log.info("Opened register post request...");
         User userExists = userService.findUserByUsername(user.getUsername());
+        log.info(userExists + " is user, that found in DB");
         if (userExists != null) {
+            log.info("userExists is not null, setting bindingResult 'username'");
             bindingResult.rejectValue("username", "Username was already registered");
         }
         if (bindingResult.hasErrors()) {
+            log.info("bindingResult has errors");
             modelAndView.setViewName("create-account-page");
         } else {
+            log.info("registering user");
             userService.createUser(user);
+            log.info("user registered");
             modelAndView.addObject("successMessage", "User has been registered successfully");
             modelAndView.addObject("user", new User());
-            modelAndView.setViewName("entrance-page");
+            log.info("sucecssMessage was set, going to login page");
+            modelAndView.setViewName("redirect:/login");
         }
 
         return modelAndView;
